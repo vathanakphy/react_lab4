c83e5eebf864c78a80347c7c86b5b905dbd9aa78
vathanakphy <vathanakphy@gmail.com>
2025-03-20 19:12:27 +0700

    fig bug

--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -5,30 +5,24 @@ export const Carousel = ({ images }) => {
   /* You will need to  use  state to mnage the current image */
   const [imgNo,setImgNo] = useState(0);
   /* You will need to hanle the click on left and right button */
-  function goLeft(e){
-    e.preventDefault();
-    setImgNo(imgNo-1);
+  function goLeft(){
+    setImgNo((n)=>(
+      (n===0)?  images.length - 1 : n-1
+    ));
   }
-  function goRight(e){
-    e.preventDefault();
-    setImgNo(imgNo+1);
+  function goRight(){
+    setImgNo((n)=>(
+      (images.length - 1 )? 0 : n+1
+    ));
   }
   /* You will need to manage the cases when we are on the last image or first image*/
-  function manageImgAccess(){
-    if(imgNo === images.length-1){
-      setImgNo(0);
-    }else if(imgNo === -1){
-      setImgNo(images.length-1);
-    }
-   }
   return (
     <div className="carousel">
       <BsArrowLeftCircleFill onClick={goLeft} className="arrow arrow-left" />
 
       {/* YOu will need to display the current image, not the first one.. */
-        manageImgAccess(imgNo)
-      }
       <img src={images[imgNo].src} alt={images[imgNo].alt} className="slide" />
+      }
 
       <BsArrowRightCircleFill onClick={goRight} className="arrow arrow-right" />
     </div>
